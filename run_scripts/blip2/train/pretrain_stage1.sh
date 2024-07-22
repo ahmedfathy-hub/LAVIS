0a2364e635afe96eec226c38a864db93714ac651
ahmedfathy <149051482+ahmedfathy-hub@users.noreply.github.com>
2024-07-22 16:19:40 +0100

    pretrain_stage1.sh

--- run_scripts/blip2/train/pretrain_stage1.sh
+++ run_scripts/blip2/train/pretrain_stage1.sh
@@ -1,1 +1,1 @@
-python -m torch.distributed.run --nproc_per_node=16 train.py --cfg-path lavis/projects/blip2/train/pretrain_stage1.yaml
+python -m torch.distributed.run --nproc_per_node=16 /content/LAVIS/train.py --cfg-path /content/LAVIS/lavis/projects/blip2/train/pretrain_stage1.yaml
